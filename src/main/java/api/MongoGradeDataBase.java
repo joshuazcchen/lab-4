fab0cd43c5f1b96dfc49154d1b9545015d0cb01d
joshuazcchen <joshuaz.chen@mail.utoronto.ca>
2025-05-29 19:06:05 -0400

    implemented GetAverageGradeUseCase and MongoGradeDataBase

--- src/main/java/api/MongoGradeDataBase.java
+++ src/main/java/api/MongoGradeDataBase.java
@@ -242,10 +242,6 @@ public class MongoGradeDataBase implements GradeDataBase {
     }
 
     @Override
-    // TODO Task 3b: Implement this method
-    //       Hint: Read the Grade API documentation for getMyTeam (link below) and refer to the above similar
-    //             methods to help you write this code (copy-and-paste + edit as needed).
-    //             https://www.postman.com/cloudy-astronaut-813156/csc207-grade-apis-demo/folder/isr2ymn/get-my-team
     public Team getMyTeam() {
         final OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
@@ -256,12 +252,27 @@ public class MongoGradeDataBase implements GradeDataBase {
                 .addHeader(CONTENT_TYPE, APPLICATION_JSON)
                 .build();
 
-        final Response response;
-        final JSONObject responseBody;
+        try {
+            final Response response = client.newCall(request).execute();
+            final JSONObject responseBody = new JSONObject(response.body().string());
 
-        // TODO Task 3b: Implement the logic to get the team information
-        // HINT: Look at the formTeam method to get an idea on how to parse the response
+            if (responseBody.getInt(STATUS_CODE) == SUCCESS_CODE) {
+                final JSONObject team = responseBody.getJSONObject("team");
+                final String teamName = team.getString("name");
+                final JSONArray teamMembers = team.getJSONArray("members");
+                final String[] members = new String[teamMembers.length()];
+                for (int i = 0; i < teamMembers.length(); i++) {
+                    members[i] = teamMembers.getString(i);
+                }
 
-        return null;
+                return new Team(teamName, members);
+            }
+            else {
+                throw new RuntimeException(responseBody.getString(MESSAGE));
+            }
+        }
+        catch (IOException | JSONException event) {
+            throw new RuntimeException(event);
+        }
     }
 }
